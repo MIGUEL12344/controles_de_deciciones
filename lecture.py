279e0a05078282878597cd9f09ed423e640f6682
miguel12344 <ronalmiguel2005@gmail.com>
2024-05-08 08:16:36 -0500

    añadi un archivo lecture a mi carpeta

---
+++ lecture.py
@@ -0,0 +1,10 @@
+# Definir una variable
+edad = 20
+
+# Control de decisiones
+if edad < 18:
+    print("Eres menor de edad.")
+elif edad >= 18 and edad < 65:
+    print("Eres un adulto.")
+else:
+    print("Eres un adulto mayor.")
